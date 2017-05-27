3026cc6a6d294632e46674b1be50fd53f5721cad
hustdhg <13377875645@163.com>
2017-05-27 21:24:51 +0800

    多线程录制弹幕

--- run.py
+++ run.py
@@ -2,35 +2,44 @@ import time, sys
 import sqlite3
 from danmu import DanMuClient
 from datetime import *
+import threading
+
 
 def pp(msg):
     print(msg.encode(sys.stdin.encoding, 'ignore').
           decode(sys.stdin.encoding))
 
 
-dmc = DanMuClient('https://www.douyu.com/dasima')
-if not dmc.isValid(): print('Url not valid')
-conn = sqlite3.connect('test.db')
-cursor = conn.cursor()
-
-
-@dmc.danmu
-def danmu_fn(msg):
+def print_dm(msg):
+    # print(msg)
     pp('[%s] %s' % (msg['NickName'], msg['Content']))
-    username = msg['NickName']
-    usercontent = msg['Content']
-    time_now=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    cursor.execute("INSERT INTO dasima VALUES (?,?)", (time_now, usercontent))
-    conn.commit()
-
-@dmc.gift
-def gift_fn(msg):
-    pp('[%s] sent a gift!' % msg['NickName'])
 
 
-@dmc.other
-def other_fn(msg):
-    pp('Other message received')
+def get_danmu_with_rid(host, rid):
+    dmc = DanMuClient(host + rid)
+    if not dmc.isValid():
+        print('Url not valid')
+    dmc.danmu(print_dm)
+    dmc.start(blockThread=True)
 
 
-dmc.start(blockThread=True)
+dy_list = ['weixiao', '1746151', 'ShinyRuo']
+
+human_list = {
+    '6666': 'pdd',
+    '10953': '君克',
+    '666666': '若风'
+}
+panda_list = ['6666', '10953']
+
+for rid in dy_list:
+    print(rid)
+    target_thread = threading.Thread(target=get_danmu_with_rid, args=('https://www.douyu.com/', rid,),
+                                     name='thread-' + rid)
+    # target_thread.setDaemon(True)
+    target_thread.start()
+
+for rid in panda_list:
+    target_thread = threading.Thread(target=get_danmu_with_rid, args=('http://www.panda.tv/', rid,),
+                                     name='thread-' + rid)
+    target_thread.start()
